82695da423ee8bcad6f941e251aa018b121c47ac
Supriya Yerramilli <supriyayerramilli@Supriyas-MacBook-Pro.local>
2019-03-04 15:08:34 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,40 @@
+
+//Config
+var applicationID = 'JE61NV9M0M';
+var apiKey = '85b9834d56f52ace03bdeb7e1293181c';
+var indexName = 'restaurants';
+
+var client = algoliasearch(applicationID, apiKey);
+var helper = algoliasearchHelper(client, indexName);
+
+//
+helper.on('result', function(content) {
+  renderHits(content);
+});
+
+
+//json response, all of the restaurannts
+// function renderHits(content) {
+//   $('#container').html(JSON.stringify(content, null, 2));
+// }
+
+//display the name of each product
+function renderHits(content){
+$('#container').html(function(){
+ return $.map(content.hits, function(hit) {
+    return '<li>' + hit._highlightResult.name.value + '<li>';
+ });
+});
+}
+
+$('#search-box').on('keyup', function() {
+ helper.setQuery($(this).val())
+    .search();
+});
+
+
+//
+
+
+//trigger the algolia search
+helper.search();
